4e17ebcca9e59873beccd63f10509062fb025f44
George Wiseman <georgeawiseman@gmail.com>
2022-05-18 15:39:01 -0700

    A character object was implemented, and in the process of working on a json file to store information when the bot is offline

---
+++ Character.py
@@ -0,0 +1,54 @@
+class Character:
+    def __init__(self, name, specific_stat : str ="None", owner:int = 0):
+        self.owner_id = owner #The discord id of whoever claimed this character
+
+        self._name = name
+        self._kills = 0
+        self._unconc = 0
+        self._deaths = 0
+        self._final_kills = 0
+        self._max_damage_dealt = 0
+        self._healing_dealt = 0
+        self._crit_success = 0
+        self._crit_fail = 0
+
+        # This leaves room for one personalized stat to take,
+        # will be decided upon creation of character, or could be changed
+        self._chara_specific_count = 0
+        self._chara_specific_type = specific_stat
+
+    def set_specific_stat(self, new_specific):
+        self._chara_specific_type = new_specific
+
+    def json_prepare(self):
+        new_dict = {"name":self._name}
+        new_dict["kills"] = self._kills
+        new_dict["unconc"] = self._unconc
+        new_dict["deaths"] = self._deaths
+        new_dict["final_kills"] = self._final_kills
+        new_dict["max_damage"] = self._max_damage_dealt
+        new_dict["healing_dealt"] = self._healing_dealt
+        new_dict["crit_success"] = self._crit_success
+        new_dict["crit_fail"] = self._crit_fail
+        new_dict["spec_count"] = self._chara_specific_count
+        new_dict["spec_type"] = self._chara_specific_type
+        new_dict["owner"] = self.owner_id
+
+        return new_dict
+
+
+def json_rebuild(input_dict) -> Character:
+    name = input_dict["name"]
+    spec_type = input_dict["spec_type"]
+    owner = input_dict["owner"]
+    new_char = Character(name, spec_type, owner)
+
+    new_char._kills = new_dict["kills"]
+    new_char._unconc = new_dict["unconc"]
+    new_char._deaths = new_dict["deaths"]
+    new_char._final_kills = new_dict["final_kills"]
+    new_char._max_damage_dealt = new_dict["max_damage"]
+    new_char._healing_dealt = new_dict["healing_dealt"]
+    new_char._crit_success = new_dict["crit_success"]
+    new_char._crit_fail = new_dict["crit_fail"]
+    new_char._chara_specific_count = new_dict["spec_count"]
--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import os
 import botlogic
 import json
 import datetime
+from Character import *
 
-doggled = True #The Doggle is the toggle to remove any messages with the annoying dog emoji when it is toggled on
+doggled = False #The "Doggle" is the toggle to remove any messages with the annoying dog emoji when it is toggled on
 bot = commands.Bot(command_prefix='!')
 bot.name = "The Broken Veil"
 
 
 @bot.event
 async def on_ready():
+
     print(f"We have logged in as {bot.name}".format())
 
 @bot.event
@@ -46,18 +48,55 @@ async def nametest(ctx, real=False):
 @bot.command(name="Doggle")
 async def blockChihuahua(ctx, switchDirection=None):
     global doggled
-    if switchDirection == None:
-        doggled = not doggled
-    elif switchDirection.lower() == "on":
+
+    if switchDirection.lower() == "on":
         doggled = True
     elif switchDirection.lower() == "off":
         doggled = False
+    else:
+        doggled = not doggled
 
     if doggled == True:
         on_off = "on"
     else:
         on_off = "off"
     await ctx.send(f"The blocking has been doggled {on_off}")
+
+@bot.command(name="Log")
+async def logStats(ctx, char_name, type, count):
+    return
+
+@bot.command(name="CreateCharacter")
+async def create_character_stats_sheet(ctx, char_name=None, char_specific_stat=None):
+    if char_name == None:
+        await ctx.send("Your Character needs a name\n Try !CreateCharacter \"Name\"")
+        return
+
+    if char_specific_stat== None:
+        await ctx.send("Your Character could use a personalized stat\n Try !CreateCharacter \"Character Name\" \"Name of Personalized Stat\"")
+        return
+
+    caller_id = ctx.author.id
+    new_char = Character(char_name, specific_stat=char_name, owner=caller_id)
+
+    with open("Characters.json", "w+") as file:
+        try:
+            file_data = json.load(file)
+        except Exception as e:
+            print(e)
+            file_data = {}
+
+        json_new_char = new_char.json_prepare()
+        print(file_data)
+
+        file_data.update(json_new_char)
+        dump = json.dumps(file_data)
+        file.write(dump)
+
+
+    await ctx.send(f"{char_name} created, with a personalized stat of {char_specific_stat}")
+    return
+
 
 key = botlogic.get_key()
 bot.run(key)
